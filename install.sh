f6f907322e79a76365852299ebdec407600a8a76
Bailey Hayes <ricochet@users.noreply.github.com>
2021-10-11 06:19:02 +0000

    fix: remove prompt for rust install

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ printf " - IGNORE_BREW     = %s\n" "${IGNORE_BREW}"
 ###
 if ! command -v 'rustc' &>/dev/null; then
     printf "\n🦀 Installing rust\n"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
 ###
